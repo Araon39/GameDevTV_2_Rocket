6667109ceaebdee81f4994607fc2ac3ee3f60076
Araon39 <andreygostev39@gmail.com>
2023-10-11 15:27:36 +0200

    Музыка2
    Добавил скрипт при нажатии на пробел музыка играет иначе стоп

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,9 +7,12 @@ public class Mover : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private float mainThrust = 100f;
     [SerializeField] private float rotateRocket = 10f;
+    [SerializeField] private AudioSource _audioSource;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,14 @@ public class Mover : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (_audioSource.isPlaying == false)
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
         }
     }
     void RotateRocket()
