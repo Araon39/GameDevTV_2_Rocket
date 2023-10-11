574d024efd1a4ef732db6d60c4e52ef4831ae0c7
Araon39 <andreygostev39@gmail.com>
2023-10-11 10:26:47 +0200

    FirstCommit
    Первый комит - самостоятельный по туториалу

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float mainThrust = 100f;
+    [SerializeField] private float rotateRocket = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRocket();
+        RotateRocket();
+    }
+
+    void MoveRocket()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+    void RotateRocket()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateRocket);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateRocket);
+        }
+    }
+
+    private void ApplyRotation(float rotationRocket)
+    {
+        rb.freezeRotation = true; //замораживаем вращения
+        transform.Rotate(Vector3.forward * rotationRocket * Time.deltaTime);
+        rb.freezeRotation = false; //размораживаем
+    }
+}
